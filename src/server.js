b07e8dbba82e05421c9afe1718e141a24b647fb1
Leandro <leandromllr9@gmail.com>
2023-11-12 16:34:02 -0300

    feat: add cors and configuring a prefix on routes

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from 'cors'
 import cfg from "./config/config.js"
 import routes from "./routes.js"
 
@@ -10,11 +11,18 @@ process.on('unhandledRejection', (reason, promise) => {
   console.error('Unhandled Rejection at:', promise, '\nReason:', reason)
 })
 
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}
+
 const main = () => {
   const server = express()
 
+  server.use(cors(corsOptions))
   server.use(express.json())
-  server.use(routes)
+  server.use('/api/v1/', routes)
 
   server.listen(cfg.port, () => {
     console.log(`Server on na porta ${cfg.port}`)
